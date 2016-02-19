d5e0777afca9303c5e5e92b3a62c10a12e889d9c
NnNnnNn <on-job@hotmail.com>
2016-02-19 11:33:49 +0700

    Fix Error install

---
+++ mod_customshow.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package     Joomla.Module
+ * @subpackage  Module.mod_customshow
+ *
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */ 
+
+defined('_JEXEC') or die;
+
+//include the helper file
+require_once __DIR__.'/helper/helper.php';
--- tmpl/default.php
+++ tmpl/default.php
@@ -5,12 +5,9 @@
  *
  * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
- */
+ */ 
+
 
 defined('_JEXEC') or die;
 
-class CustomShowHelper{
-
-
-
-}
+?>
